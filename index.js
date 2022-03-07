757508e57365851fa79913be927f9d6534400e32
abeveryday <97256547+abeveryday@users.noreply.github.com>
2022-03-06 16:13:33 -0800

    JS added to give current/accurate day & time

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-/*if (cloudy) {
-    //show image of clouds
-} else if (partially cloudy) {
-    //if condition is not true
-} else if (sunny) {
-    //ya know
-} else if (rain) {
+//document.body.style.backgroundColor = "gray"; might change background depending on time of day
+function formatDate(timestamp) {
+  //calculate the date
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
 
-} else {
-    // if none of those then generic pic
-} */
-//document.body.style.backgroundColor = "gray";
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return `${days[dayIndex]} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -17,12 +33,14 @@ function displayTemperature(response) {
   let desctriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dayElement = document.querySelector("#day");
   temperatureElement.innerHTML = Math.round(response.data.main.temp) + "°C"; //will prob be removing °C later so user can change between °C and °F
   cityElement.innerHTML = response.data.name;
   desctriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML =
     "Humidity: " + Math.round(response.data.main.humidity) + "%";
   windElement.innerHTML = Math.round(response.data.wind.speed) + "km/h";
+  dayElement.innerHTML = formatDate(response.data.dt * 1000); //have to multiply bc date info given is inaccurate without it
 }
 
 let city = "Las Vegas";
